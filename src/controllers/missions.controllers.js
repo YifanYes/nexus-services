090aec91a7fc201d244577a82f75df4ab12cbd57
Yifan Ye <yifanye@Yifans-MacBook-Pro.local>
2022-07-12 12:40:26 +0200

    Add finishMission function

--- src/controllers/missions.controllers.js
+++ src/controllers/missions.controllers.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Mission = require('../models/mission');
 const Character = require('../models/character');
+const { $where } = require('../models/mission');
 
 const addNewMission = async (req, res) => {
     const mission = new Mission({
@@ -35,6 +36,33 @@ const addNewMission = async (req, res) => {
     });
 };
 
+const finishMission = async (req, res) => {
+    let mission = await Mission.findById({ id: req.body.id });
+
+    // Marking mission as completed and registering completion time
+    mission.completed = true;
+    mission.completionTime = req.body.completionTime;
+
+    await mission.save();
+
+    // Updating each character's attribute after successfully finishing a mission
+    for (let charactedId of mission.members) {
+        let character = await Character.findById({ charactedId });
+
+        character.hp += Math.abs(mission.stress) + mission.difficulty / 4;
+        character.stress += mission.difficulty / 3;
+        character.exp +=
+            (Math.abs(mission.stress) + mission.difficulty) * 2 -
+            (mission.completionTime - mission.estimatedTime) * 5;
+        character.performance +=
+            mission.estimatedTime - mission.completionTime + 1;
+
+        await character.save();
+        // TODO: When completing 5 missions in a row, add 2 points to performance
+    }
+};
+
 module.exports = {
-    addNewMission
+    addNewMission,
+    finishMission
 };
